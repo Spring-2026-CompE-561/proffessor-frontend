85b7f5f6b860aee3965698cc98a88bcce1cc21b5
Ugur Emre Dogan <ue.dogan@gmail.com>
2026-04-13 19:53:07 -0700

    Added simple navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,67 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
 export default function Navbar() {
-	return <div>navbar</div>;
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<nav className="w-full border-b bg-slate-800 text-white ">
+			<div className="mx-auto max-w-7xl px-4">
+				<div className="flex h-16 items-center justify-between">
+					{/* Logo */}
+					<div className="text-lg font-semibold">
+						<Link href="/">Budget Buddy</Link>
+					</div>
+
+					{/* Desktop Menu */}
+					<div className="hidden md:flex items-center gap-6">
+						<Link
+							href="/"
+							className="hover:text-gray-600"
+						>
+							Home
+						</Link>
+						<Link
+							href="/about"
+							className="hover:text-gray-600"
+						>
+							About
+						</Link>
+					</div>
+
+					{/* Mobile Button */}
+					<button
+						onClick={() => setIsOpen(!isOpen)}
+						className="md:hidden text-sm border px-3 py-1 rounded"
+					>
+						Menu
+					</button>
+				</div>
+			</div>
+
+			{/* Mobile Menu */}
+			{isOpen && (
+				<div className="md:hidden px-4 pb-4 space-y-2">
+					<Link
+						href="/"
+						className="block"
+					>
+						Home
+					</Link>
+					<Link
+						href="/about"
+						className="block"
+					>
+						About
+					</Link>
+					<Link
+						href="/contact"
+						className="block"
+					>
+						Contact
+					</Link>
+				</div>
+			)}
+		</nav>
+	);
 }
